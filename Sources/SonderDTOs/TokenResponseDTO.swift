0f392a8ad2aeff4293d2d082c5a770a72d5769a0
Bryan Sample <bryanjsample@gmail.com>
2025-12-04 19:53:41 -0700

    Adds user onboarding and group status
    Adds `needsToBeOnboarded` and `userInGroup` properties to the `TokenResponseDTO`.
    
    These properties allow the client to determine whether a user needs to go through the onboarding flow, and whether the user is part of a group.

--- Sources/SonderDTOs/TokenResponseDTO.swift
+++ Sources/SonderDTOs/TokenResponseDTO.swift
@@ -6,13 +6,19 @@
 //
 
 public struct TokenResponseDTO: Codable, Sendable {
+    public let needsToBeOnboarded: Bool
+    public let userInGroup: Bool
     public let accessToken: AccessTokenDTO
     public let refreshToken: RefreshTokenDTO
 
     public init(
+        needsToBeOnboarded: Bool = false,
+        userInGroup: Bool = true,
         accessToken: AccessTokenDTO,
         refreshToken: RefreshTokenDTO
     ) {
+        self.needsToBeOnboarded = needsToBeOnboarded
+        self.userInGroup = userInGroup
         self.accessToken = accessToken
         self.refreshToken = refreshToken
     }
